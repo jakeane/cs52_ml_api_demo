d255e685a8bf7df37e64859fc0df51f5cfe30c6a
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-07-26 13:19:40 -0600

    initialize aws amplify

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div className="test">All the REACT are belong to us!</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+import App from './app';
 import './style.scss';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
-
+Amplify.configure(awsExports);
 ReactDOM.render(<App />, document.getElementById('main'));
